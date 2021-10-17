a56d00cd4807941f741fe01e13f06d11d5603053
HannaAgata <77154229+Hanna111111@users.noreply.github.com>
2021-10-17 11:52:16 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+$('.banner_slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    cssEase: 'linear',
+    autoplay: true,
+    fade: true,
+    speed: 500,
+    arrows: true,
+    slidesToScroll: 1,
+    autoplaySpeed: 4000
+});
